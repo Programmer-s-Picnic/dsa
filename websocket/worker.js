a6f63863e6e2fc6a70c2199d30f242f2f260ae57
Champak's World <champaksworld@gmail.com>
2026-06-07 11:14:59 +0530

    Update worker.js

--- websocket/worker.js
+++ websocket/worker.js
@@ -1,7 +1,18 @@
 const hostName = "Champak Roy Programmer's Picnic";
 
 export default {
-  async fetch(request) {
+  async fetch(request,env,ctx) {
+let id="not found";
+try{
+   const id = env.CHAT_ROOM.idFromName("main");
+
+      // This gives you a stub/reference to that Durable Object
+      const room = env.CHAT_ROOM.get(id);
+}
+catch(error)
+{
+  id=error;
+}
     const requestUrl = new URL(request.url);
     const upgradeHeader = request.headers.get("Upgrade");
 
@@ -17,7 +28,7 @@ export default {
       server.send("Connected to " + hostName + " WebSocket");
 
       server.addEventListener("message", (event) => {
-        server.send("Echo from " + hostName + ": " + event.data);
+        server.send(`Echo from ${ hostName} : ${ event.data} ${id} ${env} ${ctx}`);
       });
 
       server.addEventListener("close", () => {
@@ -47,3 +58,37 @@ export default {
     );
   }
 };
+
+
+export class ChatRoom {
+  constructor(state, env) {
+    this.state = state;
+    this.env = env;
+  }
+
+  async fetch(request) {
+    const upgradeHeader = request.headers.get("Upgrade");
+
+    if (!upgradeHeader || upgradeHeader.toLowerCase() !== "websocket") {
+      return new Response("Expected WebSocket request", {
+        status: 426
+      });
+    }
+
+    const pair = new WebSocketPair();
+    const client = pair[0];
+    const server = pair[1];
+  server.accept();
+
+    server.send("Connected to Durable Object");
+
+    server.addEventListener("message", (event) => {
+      server.send("Echo from Durable Object: " + event.data);
+    });
+
+    return new Response(null, {
+      status: 101,
+      webSocket: client
+    });
+  }
+}
